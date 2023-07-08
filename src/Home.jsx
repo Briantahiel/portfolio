93375ddf38bcb3c7cac561420be37577c190cd8a
BrianTahiel <briang.2012@hotmail.com>
2023-07-07 22:59:36 -0300

    minor changes

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,8 +1,42 @@
-
+import "./App.css";
 const Home = () => {
-  return (
-    <div>Home</div>
-  )
-}
+  const navbarToggle = document.querySelector(".navbar-toggle");
+  const navbarMenu = document.querySelector(".navbar-menu");
 
-export default Home
+  navbarToggle.addEventListener("click", () => {
+    navbarMenu.classList.toggle("show");
+  });
+
+  return (
+    <>
+      <nav className="navbar">
+        <div className="navbar-logo">
+          <a href="#">Logo</a>
+        </div>
+        <ul className="navbar-menu">
+          <li>
+            <a href="#">Inicio</a>
+          </li>
+          <li>
+            <a href="#">Proyectos</a>
+          </li>
+          <li>
+            <a href="#">Acerca de</a>
+          </li>
+          <li>
+            <a href="#">Contacto</a>
+          </li>
+        </ul>
+        <button className="navbar-toggle">
+          <span className="toggle-icon"></span>
+        </button>
+      </nav>
+
+      <div className="wrapper">
+        <h1 className="glitch">Glitch</h1>
+      </div>
+    </>
+  );
+};
+
+export default Home;
