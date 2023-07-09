6e633a89ef96b0975de087ee6111db2c9a64b968
BrianTahiel <briang.2012@hotmail.com>
2023-07-08 21:55:37 -0300

    minor fixes

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,42 +1,68 @@
-import "./App.css";
+
+import { useState } from 'react';
+import './App.css';
+// import parallax from "./assets/3600_2_06 (1).svg";
+
 const Home = () => {
-  const navbarToggle = document.querySelector(".navbar-toggle");
-  const navbarMenu = document.querySelector(".navbar-menu");
+  // const [parallaxTransform, setParallaxTransform] = useState({ x: 0, y: 0 });
+  // const handleMouseMove = (e) => {
+  //   const x = e.clientX / window.innerWidth;
+  //   const y = e.clientY / window.innerHeight;
+  //   setParallaxTransform({ x: x * 20, y: y * 100 });
+  // };
 
-  navbarToggle.addEventListener("click", () => {
-    navbarMenu.classList.toggle("show");
-  });
+  const [isActive, setIsActive] = useState(false);
 
+  const toggleMenu = () => {
+    setIsActive(!isActive);
+  };
   return (
     <>
-      <nav className="navbar">
-        <div className="navbar-logo">
-          <a href="#">Logo</a>
+      <div className='menu-container'>
+        <div className='menu-logo'>
+          <p>LOGO</p> 
         </div>
-        <ul className="navbar-menu">
-          <li>
-            <a href="#">Inicio</a>
-          </li>
-          <li>
-            <a href="#">Proyectos</a>
-          </li>
-          <li>
-            <a href="#">Acerca de</a>
-          </li>
-          <li>
-            <a href="#">Contacto</a>
-          </li>
-        </ul>
-        <button className="navbar-toggle">
-          <span className="toggle-icon"></span>
-        </button>
-      </nav>
-
-      <div className="wrapper">
-        <h1 className="glitch">Glitch</h1>
+        {/* <div className={`menu-links ${isActive ? 'active' : ''}`}>
+          <ul>
+            <li>HOME</li>
+            <li>ABOUT</li>
+            <li>CONTACT</li>
+          </ul>
+        </div> */}
+      <div>
+      <div className={`logo ${isActive ? 'active' : ''}`} onClick={toggleMenu}>
+      <div className="bar"></div>
+      <div className="bar"></div>
+      <div className="bar"></div>
       </div>
+      </div> 
+      </div>
+      <div className={`menu-links ${isActive ? 'active' : ''}`}>
+          <ul>
+            <li>HOME</li>
+            <li>ABOUT</li>
+            <li>CONTACT</li>
+          </ul>
+        </div>
+      {/* <div className="parallax-container" onMouseMove={handleMouseMove}>
+        <div className="parallax-container-img img" style={{ transform: `translate(${parallaxTransform.x}%, ${parallaxTransform.y}%)`, transition: "transform 0.7s ease-out" }}>
+          <img src={parallax} alt="" />  
+        </div>
+      </div> */}
+     
+      {/* <div className="parallax-container" onMouseMove={handleMouseMove}>
+      <div className="parallax-container-img img" style={{ transform: `translate(${parallaxTransform.x}%, ${parallaxTransform.y}%)`, transition: "transform 0.7s ease-out" }}>
+      
+        <div className="wrapper">
+    <h1 className="glitch"> FRONT END</h1>
+    </div>
+  </div>
+</div> */}
+<div className='social-container'>
+  
+</div>
     </>
   );
-};
+}
 
 export default Home;
