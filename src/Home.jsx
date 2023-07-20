bf1c0e14cc90db1bdf4c108d251e099f137e2964
BrianTahiel <briang.2012@hotmail.com>
2023-07-19 23:32:34 -0300

    minor fixes

--- src/Home.jsx
+++ src/Home.jsx
@@ -33,8 +33,13 @@ import calculator from "./assets/calculator.jpg";
 import portfolio from "./assets/portfolio.jpg";
 import tecnostore from "./assets/tecnoStore.jpg";
 import materialui from "./assets/materialUI.svg";
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
 
-// import bootstrapIcon from "./assets/bootstrap.png";
 
 const Home = () => {
   // useEffect(() => {
@@ -216,6 +221,27 @@ const Home = () => {
         </div>
         <br />
       </div>
+      <Card sx={{ maxWidth: 345 }}>
+      <CardMedia
+        component="img"
+        alt="green iguana"
+        height="140"
+        image="/static/images/cards/contemplative-reptile.jpg"
+      />
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="div">
+          Lizard
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+          Lizards are a widespread group of squamate reptiles, with over 6,000
+          species, ranging across all continents except Antarctica
+        </Typography>
+      </CardContent>
+      <CardActions>
+        <Button size="small">Share</Button>
+        <Button size="small">Learn More</Button>
+      </CardActions>
+    </Card>
       <div id='projects'></div>
       <div className='projects-container'>
         <h1>Projects</h1>
@@ -229,6 +255,9 @@ const Home = () => {
               </div>
               <div>
                 <a href='/'>Coming Soon</a>
+              </div>
+              <div className='proyect-description'>
+                <p></p>
               </div>
             </div>
           </div>
